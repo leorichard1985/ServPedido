43adf2a302071bb0c4de9661cfa786a832ee54d4
Leandro Conceição <leandro.vs2010@hotmail.com>
2024-07-15 21:31:47 -0300

    estrutura do projeto

---
+++ src/main/java/com/fiap/ServPedido/ServPedidoApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.ServPedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServPedidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServPedidoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/ServPedido/ServPedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.ServPedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServPedidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
